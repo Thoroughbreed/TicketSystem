b901016ed885fcc8297b4c04f6750c84270ee92b
Jan Andreasen <JAA@hartmann-packaging.com>
2022-03-21 14:22:38 +0100

    init

---
+++ TicketAPI/DAL/TicketDb.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TicketAPI.DAL;
+
+public class TicketDb : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder oB)
+    {
+        if (!oB.IsConfigured)
+        {
+            oB.UseSqlServer("Server = sdktofixdb01; Database = TicketDesk; User Id=program1; Password=smp4519");
+        }
+    }
+    
+    public TicketDb(DbContextOptions<TicketDb> options) : base(options)
+    {
+    }
+    
+//    public DbSet<Ticket> Tickets => Set<Ticket>();
+}
---
+++ TicketAPI/Program.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using TicketAPI.DAL;
+using static Microsoft.AspNetCore.Http.Results;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors();
+builder.Services.AddDbContext<TicketDb>(option => option.UseInMemoryDatabase("TicketList"));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
